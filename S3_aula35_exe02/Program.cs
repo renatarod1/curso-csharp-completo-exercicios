5b415362f87c170bcd06f8363e6a02dd9793c419
Renata Rodrigues <ticare1@hotmail.com>
2020-07-11 20:19:42 -0300

    S3 - Exercicios Estrutura Repetitiva FOR - aula 35 ate exe02

---
+++ S3_aula35_exe02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace S3_aula35_exe02
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            int IN = 0, OUT = 0;
+
+            for (int i = 0; i < n; i++) {
+                int x = int.Parse(Console.ReadLine());
+                if (x >= 10 && x <=20) {
+                    IN++;
+                } else {
+                    OUT++;
+                }
+            }
+            Console.WriteLine(IN + " in");
+            Console.WriteLine(OUT + " out");
+        }
+    }
+}
