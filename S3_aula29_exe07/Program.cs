3f15c08da93b1d6dd5c86d44a456f08f94219227
Renata Rodrigues <ticare1@hotmail.com>
2020-07-11 13:53:28 -0300

    S3 - Exercicios Estrutura Condicional - aula29 - ate exe08

---
+++ S3_aula29_exe07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace S3_aula29_exe07
+{
+    class Program
+    {
+        static void Main(string[] args) {
+                        
+            string[] vetLine = Console.ReadLine().Split(' ');
+            double x = double.Parse(vetLine[0], CultureInfo.InvariantCulture);
+            double y = double.Parse(vetLine[1], CultureInfo.InvariantCulture);
+
+            if (x == 0.0 && y == 0.0) {
+                Console.WriteLine("Origem");
+            } else if (x > 0.0 && y > 0.0) {
+                Console.WriteLine("Q1");
+            } else if (x < 0.0 && y > 0.0) {
+                Console.WriteLine("Q2");
+            } else if (x < 0.0 && y < 0.0){
+                Console.WriteLine("Q3");
+            } else {
+                Console.WriteLine("Q4");
+            }
+        }
+    }
+}
