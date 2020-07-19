5928a597f2b28f1bbe1ca34d212f6be047bbe2e9
Renata Rodrigues <ticare1@hotmail.com>
2020-07-19 18:15:25 -0300

    S5 - Construtores, this, Sobrecarga e Encapsulamento - aula60

---
+++ S5_aula60_exe01/Conta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace S5_aula60_exe01
+{
+    class Conta
+    {
+        public int Numero { get; private set; }
+        public string NomeTitular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Conta(int numero, string nomeTitular) {
+            this.Numero = numero;
+            this.NomeTitular = nomeTitular;           
+        }
+
+        public Conta(int numero, string nomeTitular, double depositoInicial) : this(numero, nomeTitular) {
+            this.Saldo = depositoInicial;
+        }
+
+        public void Deposito(double valor) {
+            this.Saldo += valor;
+        }
+
+        public void Saque(double valor) {
+            this.Saldo -= 5.0;
+            this.Saldo -= valor;
+        }
+
+        public override string ToString() {
+            return "Conta " + this.Numero + ", Titular: " + this.NomeTitular + ", Saldo: $ " +
+                this.Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ S5_aula60_exe01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace S5_aula60_exe01
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Conta conta;
+            Console.Write("Entre o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char deposito = char.Parse(Console.ReadLine());
+            if (deposito == 's' || deposito == 'S') {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta(numeroConta, nomeTitular, depositoInicial); 
+            } else {
+                conta = new Conta(numeroConta, nomeTitular);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double dep = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(dep);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            double saq = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(saq);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+        }
+    }
+}
