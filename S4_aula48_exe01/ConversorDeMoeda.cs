48875f4b5af74951929b4f5e10bbe2986335ff5f
Renata Rodrigues <ticare1@hotmail.com>
2020-07-18 22:48:05 -0300

    S4 - Classes, objetos e atributos - aula 48

---
+++ S4_aula48_exe01/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S4_aula48_exe01
+{
+    class ConversorDeMoeda
+    {
+        public static double iof = 6.0;
+
+        public static double ConversaoDollarReal(double valorDollar, double quantia) {
+            double total = valorDollar * quantia;
+            return total = total + iof * total / 100.0; 
+        }
+    }
+}
---
+++ S4_aula48_exe01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace S4_aula48_exe01
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.Write("Qual é a cotação do dólar? ");
+            double valorDollar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Valor a ser pago em reais = " + 
+                ConversorDeMoeda.ConversaoDollarReal(valorDollar, quantia).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
