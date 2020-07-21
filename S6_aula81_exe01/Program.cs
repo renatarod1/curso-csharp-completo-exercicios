021b670195c664206eef85391255a28b7d3a5e0b
Renata Rodrigues <ticare1@hotmail.com>
2020-07-21 17:59:34 -0300

    S6 - Comportamento de memória, arrays, listas - aula81

---
+++ S6_aula81_exe01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace S6_aula81_exe01
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            string[] mn = Console.ReadLine().Split(' ');
+            int m = int.Parse(mn[0]);
+            int n = int.Parse(mn[1]);
+
+            int[,] mat = new int[m, n];
+            
+            for (int i = 0; i < m; i++) {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++) {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+            
+            int search = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++) {                
+                for (int j = 0; j < n; j++) {
+                    if (mat[i,j] == search) {
+                        Console.WriteLine("Position "+ i + ","+ j);
+                        if (j != 0) {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if (i != 0) {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (n - 1 != j) {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (m - 1 != i) {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }                        
+                    }                    
+                }
+            }
+        }
+    }
+}
