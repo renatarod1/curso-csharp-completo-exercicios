d13f212f22ced8b39915cdc658e51feb2e005900
Renata Rodrigues <ticare1@hotmail.com>
2020-07-26 18:10:48 -0300

    S13 - Trabalhando com Arquivos - aula192

---
+++ S13_aula192_exe_Files_Directories/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S13_aula192_exe_Files_Directories.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product() { }
+
+        public Product(string name, double price, int quantity) {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double TotalValue() {
+            return Price * Quantity;
+        }
+    }
+}
---
+++ S13_aula192_exe_Files_Directories/Program.cs
@@ -0,0 +1,38 @@
+﻿using S13_aula192_exe_Files_Directories.Entities;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace S13_aula192_exe_Files_Directories
+{
+    class Program
+    {
+        static void Main(string[] args) {            
+            Console.Write("Enter file full path: ");
+            string sourceFilePath = Console.ReadLine();
+            string path = Path.GetDirectoryName(sourceFilePath);
+            string targetDirectoryPath = path + @"\out";           
+
+            try {
+                Directory.CreateDirectory(targetDirectoryPath);
+                string targetFilePath = targetDirectoryPath + @"\summary.csv";
+
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                using(StreamWriter sw = File.AppendText(targetFilePath)) {
+                    foreach (string line in lines) {
+                        string[] item = line.Split(",");
+                        string name = item[0];
+                        double price = double.Parse(item[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(item[2]);
+                        Product product = new Product(name, price, quantity);
+                        sw.WriteLine(product.Name + "," + product.TotalValue().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+        }
+    }
+}
