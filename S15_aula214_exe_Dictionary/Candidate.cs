3c13a3645e0adc7a76638ff51f4e85daa988682a
Renata Rodrigues <ticare1@hotmail.com>
2020-07-31 13:35:51 -0300

    S15 - Generics, Set, Dictionary - aula214 - Dictionary

---
+++ S15_aula214_exe_Dictionary/Candidate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S15_aula214_exe_Dictionary
+{
+    class Candidate
+    {
+        public string Name { get; set; }
+        public int Votes { get; set; }
+
+        public override string ToString() {
+            return Name + ": " + Votes;
+        }
+    }
+}
---
+++ S15_aula214_exe_Dictionary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace S15_aula214_exe_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Dictionary<string, int> candidates = new Dictionary<string, int>();
+            Console.Write("Enter de file full path: ");
+            string path = Console.ReadLine();
+
+            try {
+                using(StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        string[] line = sr.ReadLine().Split(",");
+                        string name = line[0];
+                        int votes = int.Parse(line[1]);
+                        Candidate candidate = new Candidate { Name = name, Votes = votes };
+                        if (candidates.ContainsKey(candidate.Name)) {
+                            candidates[candidate.Name] += candidate.Votes;
+                        } else {
+                            candidates[candidate.Name] = candidate.Votes;
+                        }
+                    }
+                }
+
+                foreach (KeyValuePair<string, int> candidate in candidates) {
+                    Console.WriteLine(candidate);
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
